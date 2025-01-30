f83f468b60f3d294b766a6775fb288336ef7bcf9
rjsaito0519 <ryuta.saito.q3@dc.tohoku.ac.jp>
2025-01-30 12:24:10 +0900

    Thu, Jan 30, 2025 12:24:10 PM

--- src/acceptance_check.cpp
+++ src/acceptance_check.cpp
@@ -157,7 +157,7 @@ Int_t main(int argc, char** argv) {
 
     // -- check argments -----
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <root file path>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <root file path> [focus_pdg_code]" << std::endl;
         return 1;
     }
     TString path = argv[1];
